ec875c5d9f216c64f27ce9954bb868641a6461e4
ne9n <ne9n.dave@gmail.com>
2020-02-18 22:16:48 -0600

    flight timer
    broke up files stated on user interface

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,14 +1,17 @@
 #include <Servo.h>
+#include <EEPROM.h>
+
+
 // create servo object to control a servo
-// state constants part of config values 
+// state constants part of config values
 
 // LIST OF THINGS TO DO
 // START BUTTON no debounce
-// Need LED support 
+// Need LED support
 // programming method
 // acclermotoer support
-// accel and decel ramps work but need a better method 
- 
+// accel and decel ramps work but need a better method
+
 
 #define MaxSpeed 120 // 0 to 180 for degrees of rotation of a servo
 #define FlyTime 90000 // air time in miliseconfs
@@ -42,12 +45,12 @@
 
 Servo esc;
 
-// definations 
+// definations
 int maxThrottle = MaxSpeed;
 int state_tmr;
-int curThrottle = 0; // current speed 
+int curThrottle = 0; // current speed
 int incTime = 0;
-int led_state =0;
+int led_state = 0;
 
 const int buttonPin = 7;
 
@@ -55,200 +58,22 @@ int fly_state = WAIT;
 int inFlight = FLYING;
 
 
-void debounce()
+
+
+void setup() 
 {
-// Variables will change:
-  static int buttonState = LOW;             // the current reading from the input pin
-  static int lastButtonState = LOW;   // the previous reading from the input pin
-
-  static unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-  static unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-
-  // read the state of the switch into a local variable:
-  int reading = digitalRead(buttonPin);
-
-  // check to see if you just pressed the button
-  // (i.e. the input went from LOW to HIGH), and you've waited long enough
-  // since the last press to ignore any noise:
-
-  // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState) 
-  {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-  if ((millis() - lastDebounceTime) > debounceDelay) 
-  {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
-
-    // if the button state has changed:
-    if (reading != buttonState) 
-    {
-      buttonState = reading;
-
-    }
-  }
-  // save the reading. Next time through the loop, it'll be the lastButtonState:
-  lastButtonState = reading;
-  return !buttonState;
-}
-
-
-
-
-
-void setup() {
-	// attaches the servo on pin 9 to the servo object
-	esc.attach(9, 1000, 2000);
-	esc.write(curThrottle);
-  pinMode(buttonPin, INPUT); 
+  // attaches the servo on pin 9 to the servo object
+  esc.attach(9, 1000, 2000);
+  esc.write(curThrottle);
+  pinMode(buttonPin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 }
 
-void ledUpdate()
-{
-  unsigned long currentMillis1 = millis();
 
-  switch (led_state)
-  {
-    case 1:
-    {
-      digitalWrite(LED_BUILTIN, HIGH);
-      led_state = 2;
-      break;  
-    }
-    case 2:
-    {
-      digitalWrite(LED_BUILTIN, LOW);
-      led_state = 1;
-      break;  
-     
-    }
-  }
-}
-void flyState()
-{
-unsigned long currentMillis1 = millis();
 
-  switch (inFlight)
-  {
-    case FLYING:
-    {
-     if ( (currentMillis1 - state_tmr) > FlyTime)
-      {
-        //fly_state = RAMPDWN;
-        state_tmr = millis();
-        inFlight= BURP;
-      }
-      break;
-    }
-    case BURP:
-    {
-      curThrottle = BurpMax;
-      if (currentMillis1 - state_tmr > BURPTIME)
-      {
-        curThrottle = MaxSpeed;
-        state_tmr = 0;
-        inFlight = RDYLAND;
-      }
-      break;
-    }
-    case RDYLAND:
-    {
-      if((currentMillis1 - state_tmr) > RDYTIME)
-      {
-        state_tmr = 0;
-        fly_state = RAMPDWN; 
-      }
-    }
-  }
-}
-void speedState() 
-{
- unsigned long currentMillis = millis();
- 
- switch (fly_state)
- {
-    case WAIT:
-    {
-      // need to see a low to high transistion 
-      if (digitalRead(buttonPin))
-      {
-        state_tmr = millis();
-        curThrottle = 0;
-        fly_state = ARMED;
-      }
-      break;
-    }
-    case ARMED:
-    {
-      if ( currentMillis- state_tmr > ArmTime)
-      {
-        fly_state = TAKEOFF;
-        curThrottle = 0;
-        incTime = currentMillis;
-        state_tmr = millis();
-      }   
-      break;
-    }
-    case TAKEOFF:
-    {
-      Serial.print(curThrottle, DEC);
-      Serial.print("\n");
-      if (curThrottle  >= MaxSpeed)
-      {
-        fly_state = FLY;
-        state_tmr = millis();
-        curThrottle = MaxSpeed;
-        
-      }        
-      else if (currentMillis- incTime >INCTIME)
-      {
-        curThrottle = curThrottle+ IncThrottle;
-        incTime = currentMillis;
-      }
 
-      break;
-    }
-    case FLY:
-    {
-      flyState();
-      incTime = currentMillis;
-      break;
-    }
-    case RAMPDWN:
-    {
 
-      if ((curThrottle)<=10 )
-      {
-        fly_state = LAND;
-        curThrottle = 0;  
-        incTime = currentMillis;
-      }
-      else if (currentMillis- incTime >INCTIME)
-      {
-        curThrottle= curThrottle - IncThrottle; 
-        incTime = currentMillis;
-      }
-      break;
-    }
-    case LAND:
-    default:
-    {
-      curThrottle = 0;
-      break;
-    }
- }
- if (fly_state < LAND)
- {
- } 
- Serial.print(curThrottle, DEC);
- Serial.print("\n");
-
- esc.write(curThrottle);
-}
 void loop()
 {
   speedState();
---
+++ firmware/state_machine.ino
@@ -0,0 +1,127 @@
+
+void flyState()
+{
+  unsigned long currentMillis1 = millis();
+
+  switch (inFlight)
+  {
+    case FLYING:
+      {
+        if ( (currentMillis1 - state_tmr) > FlyTime)
+        {
+          //fly_state = RAMPDWN;
+          state_tmr = millis();
+          inFlight = BURP;
+        }
+        break;
+      }
+    case BURP:
+      {
+        curThrottle = BurpMax;
+        if (currentMillis1 - state_tmr > BURPTIME)
+        {
+          curThrottle = MaxSpeed;
+          state_tmr = 0;
+          inFlight = RDYLAND;
+        }
+        break;
+      }
+    case RDYLAND:
+      {
+        if ((currentMillis1 - state_tmr) > RDYTIME)
+        {
+          state_tmr = 0;
+          fly_state = RAMPDWN;
+        }
+      }
+  }
+}
+
+
+void speedState()
+{
+  unsigned long currentMillis = millis();
+
+  switch (fly_state)
+  {
+    case WAIT:
+      {
+        // need to see a low to high transistion
+        if (digitalRead(buttonPin))
+        {
+          state_tmr = millis();
+          curThrottle = 0;
+          fly_state = ARMED;
+        }
+        else
+        {
+
+        }
+        break;
+      }
+    case ARMED:
+      {
+        if ( currentMillis - state_tmr > ArmTime)
+        {
+          fly_state = TAKEOFF;
+          curThrottle = 0;
+          incTime = currentMillis;
+          state_tmr = millis();
+        }
+        break;
+      }
+    case TAKEOFF:
+      {
+        Serial.print(curThrottle, DEC);
+        Serial.print("\n");
+        if (curThrottle  >= MaxSpeed)
+        {
+          fly_state = FLY;
+          state_tmr = millis();
+          curThrottle = MaxSpeed;
+
+        }
+        else if (currentMillis - incTime > INCTIME)
+        {
+          curThrottle = curThrottle + IncThrottle;
+          incTime = currentMillis;
+        }
+
+        break;
+      }
+    case FLY:
+      {
+        flyState();
+        incTime = currentMillis;
+        break;
+      }
+    case RAMPDWN:
+      {
+
+        if ((curThrottle) <= 10 )
+        {
+          fly_state = LAND;
+          curThrottle = 0;
+          incTime = currentMillis;
+        }
+        else if (currentMillis - incTime > INCTIME)
+        {
+          curThrottle = curThrottle - IncThrottle;
+          incTime = currentMillis;
+        }
+        break;
+      }
+    case LAND:
+    default:
+      {
+        curThrottle = 0;
+        break;
+      }
+  }
+  if (fly_state < LAND)
+  {
+  }
+ 
+
+  esc.write(curThrottle);
+}
