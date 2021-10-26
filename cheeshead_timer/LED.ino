356261712237a6784b6d7f224a5669c546eeebb9
ne9n <59241873+ne9n@users.noreply.github.com>
2021-10-26 08:31:59 -0500

    Update LED.ino
    big changes use LED object, changed LED update function to object 
    will project will not compile as is.

--- cheeshead_timer/LED.ino
+++ cheeshead_timer/LED.ino
@@ -4,6 +4,8 @@
  */
 
 #include<NoDelay.h>
+#include "Led.h"
+#include "Button.h"
 
 #define LED3 10
 #define LED4 11
@@ -30,16 +32,12 @@ noDelay LEDtime(250);//Creats a noDelay varible set to 1000ms
 
 void led_init()
 {
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
-  lights[0].pin = LED3;
-  lights[1].pin = LED4;
-  lights[2].pin = LED5;
  
-  lights[0].skip = 1;
-  lights[1].skip = 1;
-  lights[2].skip = 1;
+ Led led1(LED3);
+ Led led2(LED4);
+ Led led3(LED5);
+ /* change this to external button*/
+ Button button1(BUTTON_PIN); 
   
 
 }
@@ -53,59 +51,13 @@ void ledUpdate()
   static byte ct;
   byte i;
   if(LEDtime.update())//Checks to see if set time has past
-  { 
-    ct++;
-    if (ct>4)
-      ct = 0;
-
-    for (i = 0; i<3; i++)
-    {
-    
-       {
-        if (ct & lights[i].skip )
-        {
-            digitalWrite(lights[i].pin,lights[i].toggle);
-            if(lights[i].toggle == HIGH)
-                lights[i].toggle = LOW;
-            else
-                lights[i].toggle = HIGH;
-        }
-      }  
-    }
+  {
+   /* this jsut has flashing updates from LED object */
   }
+ 
 }
 
 void ledTest()
 {
-   static bool x;  
-    if (lights[0].toggle== HIGH)
-  
-      lights[0].toggle= LOW;
-     
-    else
-      lights[0].toggle = HIGH;
-      
-    digitalWrite(lights[0].pin, lights[0].toggle);
-        
-    lights[0].toggle !=lights[0].toggle; 
 
-
- /*   for (i = 0; i<4; i++)
-    {
-
-          digitalWrite(lights[i].pin,lights[i].toggle*lights[i].invert);
-          lights[i].toggle !=lights[i].toggle; 
-        
-    }*/
-
-   /* digitalWrite(LED3, HIGH);
-    digitalWrite(LED4, LOW);
-    digitalWrite(LED5, HIGH);
-
-    delay(200);
- 
-    digitalWrite(LED3, LOW);
-    digitalWrite(LED4, HIGH);
-    digitalWrite(LED5, LOW);
-*/
 }
