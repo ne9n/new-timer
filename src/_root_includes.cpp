38838a28c4849fbe5cd363e8bdb0c5c0103ea970
ne9n <59241873+ne9n@users.noreply.github.com>
2026-05-16 16:57:29 -0500

    Delete src directory

--- src/_root_includes.cpp
+++
@@ -1,8 +0,0 @@
-// Include legacy .cpp files from project root so PlatformIO compiles them
-#include "../ButMenu.cpp"
-#include "../gyro.cpp"
-#include "../led.cpp"
-#include "../led2.cpp"
-#include "../state_machine.cpp"
-#include "../state_macine.cpp"
-#include "../term.cpp"
